981c5a3a73671ae2cd5c083552a0e64e02e48740
Jordan Featherstone <jordanfeatherstone@gmail.com>
2018-02-23 16:07:50 +0000

    Having issues with serializer.py file

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -2,7 +2,25 @@ from django.contrib.auth import authenticate, login, get_user_model
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ContactForm, LoginForm, RegisterForm
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 def home_page(request):
     context = {
